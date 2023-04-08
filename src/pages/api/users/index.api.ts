1023407906cf57fe78ad3380a98c251ca5afbba4
NatanRei <nreischmura@gmail.com>
2023-04-07 22:45:51 -0300

    feat: set cookie of user in cad process

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,6 @@
-import { prisma } from '@/lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
+import { prisma } from '@/lib/prisma'
 
 export default async function handler(
   req: NextApiRequest,
@@ -26,6 +27,11 @@ export default async function handler(
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@ntncall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
   })
 
   return res.status(201).json(user)
