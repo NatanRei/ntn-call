960aea68e42c48915be1dc0f2c59aa0095e761b1
NatanRei <nreischmura@gmail.com>
2023-04-07 22:36:33 -0300

    feat: saving a new user

---
+++ src/pages/api/users/index.api.ts
@@ -0,0 +1,32 @@
+import { prisma } from '@/lib/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const { name, username } = req.body
+
+  const userExists = await prisma.user.findUnique({
+    where: { username },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'User already exists.',
+    })
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      username,
+    },
+  })
+
+  return res.status(201).json(user)
+}
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -6,6 +6,8 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '@/lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -41,7 +43,18 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err?.response?.data?.message)
+        return
+      }
+      console.log(err)
+    }
   }
   return (
     <Container>
