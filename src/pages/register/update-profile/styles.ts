5576ee8093054eaffed20a3b457b69c35ea683e7
NatanRei <nreischmura@gmail.com>
2023-04-09 16:37:02 -0300

    feat: name and avatar in profile page

--- src/pages/register/update-profile/styles.ts
+++ src/pages/register/update-profile/styles.ts
@@ -1,6 +1,17 @@
 import { Box, Text, styled } from '@ntn-ui/react'
 
-export const ProfileBox = styled(Box, {})
+export const ProfileBox = styled(Box, {
+  marginTop: '$6',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$4',
+
+  label: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '$4',
+  },
+})
 
 export const FormAnnotation = styled(Text, {
   color: '$gray200',
